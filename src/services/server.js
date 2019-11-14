ac8fa1d8dee06b0fa5778f7b8224922bdc88a0b9
richie1982 <richie.dowland@gmail.com>
2019-11-14 18:12:16 +0000

    updates

--- src/services/server.js
+++ src/services/server.js
@@ -3,11 +3,13 @@ import io from 'socket.io-client'
 let socket
 
 if (!socket) {
-    socket = io(':3001')
+    socket = io(':3030')
     socket.on('chat-message', msg => {
         console.log({msg})
     })
+
 }
+socket.close()
 
 export const sendChat = (user, msg) => {
     const obj = {
